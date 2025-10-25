bb8c357bed8a46087b2ca0e0f9abfd64efcb881d
Muhammad Mohid Tauqeer <mohidtauqeer8@gmail.com>
2025-10-25 13:42:29 +0500

    Add files via upload

---
+++ programmind day 4/problem2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int speed;
+    cout<<"Enter your car speed=";
+    cin>>speed;
+    if(speed>100)
+    cout<<"Halt...you will be challaned";
+    else 
+    cout<<"you are going good";
+    
+    return 0;
+}
